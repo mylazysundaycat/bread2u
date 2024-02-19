0fc48c0c679e9067fc00600ed61d9402483f41d1
Lazy Cat <mylazysundaycat@gmail.com>
2024-02-19 22:27:44 +0900

    Initial init

---
+++ src/main/java/com/daegeon/bread2u/Bread2uApplication.java
@@ -0,0 +1,13 @@
+package com.daegeon.bread2u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bread2uApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bread2uApplication.class, args);
+	}
+
+}
