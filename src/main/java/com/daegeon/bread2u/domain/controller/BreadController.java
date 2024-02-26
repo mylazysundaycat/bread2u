e2d5c3fd5afc09c06e115743dddf941714bcf1ad
Lazy Cat <mylazysundaycat@gmail.com>
2024-02-26 17:28:04 +0900

    refactor: bread CRUD 완료 ThymeLeaf UI와 연결하여 데이터베이스 체크까지 완료

--- src/main/java/com/daegeon/bread2u/domain/controller/BreadController.java
+++ src/main/java/com/daegeon/bread2u/domain/controller/BreadController.java
@@ -1,6 +1,7 @@
 package com.daegeon.bread2u.domain.controller;
 
 import com.daegeon.bread2u.domain.entity.Bread;
+import com.daegeon.bread2u.domain.repository.dto.BreadDto;
 import com.daegeon.bread2u.domain.service.BreadService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -19,18 +20,19 @@ public class BreadController {
     private final BreadService breadService;
 
 
-    @GetMapping("/bread")
-    @Operation(summary = "빵 작성 페이지", description = "빵 게시물 작성 페이지로 이동한다")
-    public String createBread() {
-        return "/BreadForm";
+    @GetMapping("/breadList/create")
+    @Operation(summary = "빵 등록 페이지", description = "빵 등록 페이지로 이동한다")
+    public String createBread(Model model) {
+        model.addAttribute("bread", new BreadDto());
+        return "/bread/createBreadForm";
     }
 
-    @PostMapping("/bread")
-    @Operation(summary = "빵 작성", description = "빵 게시물을 작성한다")
+    @PostMapping("/breadList/create")
+    @Operation(summary = "빵 등록", description = "빵을 등록한다.")
     public String createBread(@ModelAttribute Bread bread) {
         Bread createdBread = breadService.createBread(bread);
-//        return "ok";
-        return "redirect:/"+createdBread.getId();
+        return "redirect:/breadList";
+        //return "redirect:/"+createdBread.getId();
     }
 
     //find
@@ -51,9 +53,8 @@ public class BreadController {
     @GetMapping("/bread/{breadId}/update")
     @Operation(summary = "빵 수정 페이지", description = "빵 게시물 수정 페이지로 이동한다")
     public String updateBread(@PathVariable Long breadId, Model model) {
-
-        model.addAttribute("Bread", findById(breadId));
-        return "/BraedForm";
+        model.addAttribute("bread", findById(breadId));
+        return "/bread/updateBreadForm";
     }
 
     @PostMapping("/bread/{breadId}/update")
@@ -63,17 +64,17 @@ public class BreadController {
         Bread updatedBread = breadService.updateBread(breadId, bread);
         redirectAttributes.addAttribute("breadId", updatedBread.getId());
         redirectAttributes.addFlashAttribute("message", "빵이 수정되었습니다.");
-//        return "ok";
-        return "redirect:/"+updatedBread.getId();
+        return "redirect:/breadList";
+//        return "redirect:/"+updatedBread.getId();
     }
 
     @GetMapping("/bread/{breadId}/delete")
     @Operation(summary = "빵 삭제", description = "id에 해당하는 빵 게시물을 삭제한다.")
-    public String deleteBread(@PathVariable Long breadId, RedirectAttributes redirectAttributes){
+    public String deleteBread(@PathVariable(name="breadId") Long breadId, RedirectAttributes redirectAttributes){
         breadService.deleteBread(findById(breadId));
         redirectAttributes.addFlashAttribute("message", "빵이 삭제되었습니다.");
 //        return "ok";
-        return "redirect:/";
+        return "redirect:/breadList";
     }
 
 
--- src/main/java/com/daegeon/bread2u/domain/repository/dto/BreadDto.java
+++ src/main/java/com/daegeon/bread2u/domain/repository/dto/BreadDto.java
@@ -6,28 +6,17 @@ import com.daegeon.bread2u.domain.entity.Member;
 import com.daegeon.bread2u.domain.entity.Shop;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.autoconfigure.graphql.ConditionalOnGraphQlSchema;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Data
-@Builder
-@Transactional
+@Getter
+@Setter
 public class BreadDto {
-
-    private Long id;
-
     private String title;
-
     private Long price;
-
     private String detail;
-
-    private Shop shop;
-
-    private Member member;
-
-    private List<Comment> comments;
-
 }
