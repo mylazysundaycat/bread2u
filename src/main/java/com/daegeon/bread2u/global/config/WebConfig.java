2bd67f024ca011264196108dff257856817549f5
Lazy Cat <mylazysundaycat@gmail.com>
2024-12-22 02:13:35 +0900

    refactor: web cors 설정

--- src/main/java/com/daegeon/bread2u/global/config/WebConfig.java
+++ src/main/java/com/daegeon/bread2u/global/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.daegeon.bread2u.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,14 +10,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
+    @Value("${origin_url}")
+    private String url;
     public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+    public static final String ALLOWED_HEADERS = "DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range";
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8080")
+                .allowedOrigins(url)
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .allowedHeaders(ALLOWED_HEADERS.split(","))
                 .allowCredentials(true);
     }
 }
